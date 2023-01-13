06567921dc4dabc983f0b6eefaccae3ea30edfde
Madeline <madelinejgowan@gmail.com>
2023-01-13 10:08:58 -0800

    we figured it out! maddie: 1, student directory: 0

--- script/page8.js
+++ script/page8.js
@@ -1,7 +1,11 @@
 let nameInput = document.getElementById("nameInput");
 let helloReturn = document.getElementById("helloReturn");
+let lastReturn = document.getElementById("lastReturn");
+let slackReturn = document.getElementById("slackReturn");
+let emailReturn = document.getElementById("emailReturn");
+let hobbiesReturn = document.getElementById("hobbiesReturn");
 let helloSubBtn = document.getElementById("helloSubBtn");
-//console.log(nameInput, helloReturn, helloSubBtn);
+console.log(nameInput, helloReturn, helloSubBtn);
 
 let savedInput = "";
 let sayHelloUrl = "";
@@ -12,11 +16,15 @@ helloSubBtn.addEventListener("click", function () {
 
 function urlCall(url) {
     fetch(url).then(
-        response => response.text()
+        response => response.json()
     ).then(
         data => {
-            helloReturn.textContent = data;
-            console.log(data.firstName);
+            helloReturn.textContent = "First Name: " + data.firstName;
+            lastReturn.textContent = "Last Name: " + data.lastName;
+            slackReturn.textContent = "Slack Name: " + data.slackName;
+            emailReturn.textContent = "Email: " + data.email;
+            hobbiesReturn.textContent = "Hobbies: " + data.hobbies;
+            console.log(data.firstName, data.lastName, data.slackName, data.email, data.hobbies);
         }
     )
 }
